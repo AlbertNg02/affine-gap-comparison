e8aa7982a5483626d584103d919d315caf819fce
Albert_Nguyen <77342590+AlbertNg02@users.noreply.github.com>
2023-03-23 22:13:57 -0500

    Modified ReadMe

--- shellScripts/multi_echo.sh
+++ shellScripts/multi_echo.sh
@@ -1,6 +1,10 @@
 
 #!/bin/bash
 
+./echo_python.sh ../data/BRCA1_score_0.txt ../data/BRCA1-partial.txt ../data/BRCA1-exons.txt global
+
+./echo_python.sh ../data/BRCA1_score_1.txt ../data/BRCA1-partial.txt ../data/BRCA1-exons.txt global
+
 ./echo_python.sh ../data/TP53_score.txt ../data/TP53-Rat.fasta ../data/TP53-Cat.fasta global
 
 ./echo_python.sh ../data/TP53_score.txt ../data/TP53-Rat.fasta ../data/TP53-Cat.fasta local
